4cb66e252bf1d947f3d8f075986a6138bd12374e
jiaying.lu <lujiaying93@foxmail.com>
2015-06-08 09:55:41 +0800

    Add logger things

--- flask_app/app.py
+++ flask_app/app.py
@@ -35,14 +35,14 @@ def senzCollectorAPI():
     try:
         incoming_data = json.loads(request.data)
     except ValueError, err_msg:
-        #logger.error('[ValueError] err_msg: %s, params=%s' % (err_msg, request.data))
+        logger.error('[ValueError] err_msg: %s, params=%s' % (err_msg, request.data))
         result['message'] = 'Unvalid params: NOT a JSON Object'
         return json.dumps(result)
 
     # params key checking
     for key in ['filter', 'timelines']:
         if key not in incoming_data:
-            #logger.error("[KeyError] params=%s, should have key: %s" % (incoming_data, key))
+            logger.error("[KeyError] params=%s, should have key: %s" % (incoming_data, key))
             result['message'] = "Params content Error: cant't find key=%s" % (key)
             return json.dumps(result)
 
@@ -51,7 +51,7 @@ def senzCollectorAPI():
         result['code'] = 0
         result['message'] = 'success'
     except Exception, e:
-        #logger.error('[Exception] generate result error: %s' % (str(e)))
+        logger.error('[Exception] generate result error: %s' % (str(e)))
         result['code'] = 0
         result['message'] = '500 Internal Error'
         return json.dumps(result)
